c1837d67435c9e00d9f49cb50e85ba230a7f339b
Doğaç Ulğaç <ulgac1907@gmail.com>
2026-04-28 21:10:24 +0300

    Add order service database configuration to docker-compose

---
+++ order-service/src/test/java/com/dogac/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogac.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
